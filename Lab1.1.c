bc0c4cef57da495ff822e39d585a2147879fd65a
Hannibelz <ccckittiput@gmail.com>
2025-11-20 14:34:54 +0700

    Lab1.1

---
+++ Lab1.1.c
@@ -0,0 +1,18 @@
+// Pre-code (โค้ดบังคับ)
+#include <stdio.h>
+
+int main() {
+    int N, i;
+
+    // รับค่า N (จำนวนรอบ)
+    if (scanf("%d", &N) != 1) {
+            printf("Error Please Try again");
+        return 1;// Handle input failure
+    }else{
+      for(i=0; i<N ;i++){
+        printf("Hello Loop!\n");
+        }
+    }
+    // TODO: Implement the for loop to print the message N times
+    return 0;
+}
