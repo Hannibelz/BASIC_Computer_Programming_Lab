1dc90b576dba169dd74a535fa0511d25f42d2856
Hannibelz <ccckittiput@gmail.com>
2025-11-25 01:13:52 +0700

    Lab1.3

--- Lab1.3.c
+++ Lab1.3.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 
 int main() {
+
     int num1, num2, operationCode, result ;
 
     printf( "Please Insert Your Number ( num1, num2, OperationCode ) : " ) ;
     if (scanf( "%d %d %d", &num1, &num2, &operationCode ) != 3 ) {
         return 1 ;
     }
-    switch ( operationCode ) {
+    switch ( operationCode )
+    {
     case 1:
         result = num1 + num2 ;
-        break ;
-    
-    case 2:
-        result = num1 - num2 ;
+        printf( "Result = %d ", result ) ;
         break ;
 
-    case 3:
-        result = num1 * num2 ;
+    case 2:
+        result = num1 - num2 ;
+        printf( "Result = %d ", result ) ;
         break ;
     
+    case 3:
+        result = num1 * num2 ;
+        printf( "Result = %d ", result ) ;
+        break ;
+
     case 4:
-        result = num1 / num2 ;
+        if ( num2 == 0 ) {
+            printf( "Error cannot Divided By 0 " ) ;
+        }else {
+            result = num1 / num2 ;
+            printf( "Result = %d ", result ) ;
+        }
         break ;
 
     default:
+        printf( "Invalid Operation \n" ) ;
     }
     return 0 ;
 }
